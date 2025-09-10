89520a1aa1e29efac70748babc0c4de069cb93ec
Maximizer02 <max.beller@yahoo.com>
2025-09-11 01:28:59 +0200

    move html to new generic system

---
+++ bf2html.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+SOURCE=$1;
+if [ ! -f "$SOURCE" ] ; then
+		echo "Usage: bf2c [source.bf]";
+		exit 65;
+fi
+CONFIG="$PWD/configs/config-html.edn";
+clojure -M bft.clj "$SOURCE" "$CONFIG";
--- bf2html/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-clojure -M bf2html.clj main.html "$(cat $1)"
