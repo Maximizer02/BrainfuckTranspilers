4d67dad9a2fc695ce04762541979c6156d99781e
Maximizer02 <max.beller@yahoo.com>
2025-09-11 00:54:40 +0200

    move rust transpiler to generic system

---
+++ bf2rs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+SOURCE=$1;
+if [ ! -f "$SOURCE" ] ; then
+		echo "Usage: bf2rs [source.bf]";
+		exit 65;
+fi
+CONFIG="$PWD/configs/config-rs.edn";
+FILE=$(clojure -M bft.clj "$SOURCE" "$CONFIG");
+OUT=${FILE%.*};
+rustc -o "$OUT" -C opt-level=3 "$FILE"
+chmod +x "$OUT";
+./"$OUT";
+rm "$FILE" "$OUT";
--- bf2rs/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-clojure -M bf2rs.clj main.rs "$(cat $1)"
-rustc -o main -C opt-level=3 main.rs
-chmod 777 main
-./main
-rm main main.rs
